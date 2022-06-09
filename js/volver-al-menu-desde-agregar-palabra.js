e5454eafe69151b1514d1fc7f1d9b316bd360442
MauroPeralta1595 <101117763+MauroPeralta1595@users.noreply.github.com>
2022-06-09 13:37:21 -0300

    funcionalidad para limpiar opciones ingresadas en menú agregar- palabra

--- js/volver-al-menu-desde-agregar-palabra.js
+++ js/volver-al-menu-desde-agregar-palabra.js
@@ -4,5 +4,12 @@ BOTON_VOLVER_AL_MENU_2.addEventListener("click",function(){
     
     MENU_PRINCIPAL.classList.remove("ocultar");
     MENU_AGREGAR_PALABRA.classList.add("ocultar");
-
+    categoriaElegida = 3;
+    BOTON_PROGRAMACION.classList.remove('seleccionado');
+    BOTON_COMIDAS.classList.remove('seleccionado');
+    BOTON_PAIS.classList.remove('seleccionado');
+    BOTON_GENERAL.classList.remove('seleccionado');
+    PALABRA_INGRESADA.value = "";
+    TEXTO_INFORMATIVO.classList.remove('error');
+    PALABRA_INGRESADA.classList.remove('error');
 })
